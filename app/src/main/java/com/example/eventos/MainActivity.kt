6ca26b1ae9adfa9b46751aa01e75f734e2f285b4
Rafael <rfmachado91@gmail.com>
2024-05-18 21:28:03 -0300

    Corrige API

--- app/src/main/java/com/example/eventos/MainActivity.kt
+++ app/src/main/java/com/example/eventos/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,15 +13,17 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.eventos.api.EventsItem
 import com.example.eventos.api.asyncGetHttpRequest
@@ -46,47 +50,53 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GetEventsItems(modifier: Modifier = Modifier) {
     val eventList = remember { mutableStateOf<List<EventsItem>>(listOf()) }
-    Column {
-        Button(onClick = {
-            asyncGetHttpRequest(
-                endpoint = "https://events.wiremockapi.cloud/api/events",
-                onSuccess = {
-                    eventList.value = it.response.eventos
-                },
-                onError = {
-                    Log.d("ERROR", it.message.toString())
-                }
-            )
-        }) {
-            Text(
-                text = "Buscar eventos",
-                modifier = modifier
-            )
+    var isLoading = remember { mutableStateOf(true) }
+
+    asyncGetHttpRequest(
+        endpoint = "https://events.wiremockapi.cloud/api/events",
+        onSuccess = {
+            isLoading.value = false
+            eventList.value = it.response.eventos
+        },
+        onError = {
+            isLoading.value = false
+            Log.d("ERROR", it.message.toString())
         }
+    )
 
-        LazyColumn(
-            modifier = Modifier.fillMaxSize(),
-            contentPadding = PaddingValues(16.dp)
-        ) {
-            items(eventList.value) { item ->
-                Card(
-                    modifier = Modifier
-                        .padding(16.dp)
-                        .fillMaxWidth()
-                ) {
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(16.dp)
+    ) {
+        items(eventList.value) { item ->
+            Card(
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth()
+            ) {
 
-                    Column {
-                        Text(
-                            text = "Titulo : ${item.title}",
-                            modifier = modifier
-                        )
-                        Text(
-                            text = "Descrição: ${item.description}",
-                            modifier = modifier
-                        )
-                    }
+                Column {
+                    Text(
+                        text = "Titulo : ${item.title}",
+                        modifier = modifier
+                    )
+                    Text(
+                        text = "Descrição: ${item.description}",
+                        modifier = modifier
+                    )
                 }
             }
+        }
+    }
+
+    if (isLoading.value) {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White)
+        ) {
+            CircularProgressIndicator()
         }
     }
 }
